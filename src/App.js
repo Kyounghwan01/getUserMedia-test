39ddd03edd5bc464e4c599520738ed4688c5cb93
kyounghwan Noh <noh5524@gmail.com>
2022-12-27 06:36:06 +0000

    8장

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export const WEBRTC_QUALITY = {
 const Index = () => {
 	const videoRef = useRef(null);
 	const canvasRef = useRef(null);
-	const [canvasImage, setCanvasImage] = useState('');
+	const [canvasImage, setCanvasImage] = useState([]);
 	const [screenWidth, setScreenWidth] = useState(0);
 	const [screenHeight, setScreenHeight] = useState(0);
 	// const [currentStep, setCurrentStep] = useState(1);
@@ -44,14 +44,13 @@ const Index = () => {
 
 	// eslint-disable-next-line
 	useEffect(() => {
-		setScreenHeight(window.innerHeight);
-		setScreenWidth(window.innerWidth);
+		setScreenHeight(window.innerWidth / 3);
+		setScreenWidth(window.innerWidth /3 );
 		test();
 		// eslint-disable-next-line
 	}, []);
 
 	const test = async () => {
-
 		try {
 			const stream = await navigator.mediaDevices.getUserMedia({
 				audio: false,
@@ -80,8 +79,8 @@ const Index = () => {
 		context.drawImage(videoRef.current, 0, 0, screenWidth, screenHeight);
 
 		const data = canvasRef.current.toDataURL('image/png');
-
-		setCanvasImage(data);
+		const imgList = [...canvasImage, data];
+		setCanvasImage(imgList);
 		console.log(data);
 	};
 
@@ -103,22 +102,22 @@ const Index = () => {
 		{/* <button onClick={testFunc}>teststetset</button> */}
 			<div className="camera">
 				{/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-				<video ref={videoRef} autoPlay playsInline id="video" style={{ width: '100vw', height: '80vh', objectFit: 'fill' }}>
+				<video ref={videoRef} autoPlay playsInline id="video" style={{ width: '30vw', height: '30vw', objectFit: 'fill' }}>
 					Video stream not available.
 				</video>
 
-				<div style={{ height: '100px' }}>wdwd</div>
 				<canvas width={screenWidth} height={screenHeight} ref={canvasRef} id="canvas" style={{ display: 'none' }} />
-				{canvasImage && <img alt="ddd" src={canvasImage} height={screenHeight} width={screenWidth} />}
-				<div className="camera-inner">
-					<div>1/8</div>
-					<div>조수석 옆면 촬영</div>
-					<div>옆면 전체가 잘 보이도록...</div>
-					https://github.com/mdn/samples-server/blob/master/s/webrtc-capturestill/index.html
-					<button onClick={captureVideo} style={{ padding: '10px', background: 'red' }}>
+				{canvasImage.length && (
+					canvasImage.map(img => {
+						return (
+							<img alt="ddd" src={img} height={screenHeight} width={screenWidth} />
+						)
+					})
+				)}
+				
+				<button onClick={captureVideo} style={{ padding: '10px', background: 'red' }}>
 						Take photo
 					</button>
-				</div>
 			</div>
 			<canvas id="canvas"> </canvas>
 		</>
